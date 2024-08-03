693ff2d6dd88c378d1985723f67f8ce80ebaca9f
Arukatlausha <arukatlaushareddy9966@gmail.com>
2024-08-03 13:22:47 +0530

    first commit

---
+++ src/test/java/com/pilotproject/projectserver/ProjectserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilotproject.projectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
